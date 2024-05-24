9fe928012a8c972f0e26ca8b2b1336f0f62835da
StivAM <eragma05@gmail.com>
2024-05-24 13:40:27 -0600

    Commit Portafolio de CI

---
+++ src/pkg2024_itess_tics_ti202_ci_portafolio/Main.java
@@ -0,0 +1,16 @@
+/*
+ * ITESS - TICS
+ * Enero-Junio 2024
+ * Calculo integral portafolio
+ * TI-202
+ * 24/05/2024
+ * by Erik
+ */
+package pkg2024_itess_tics_ti202_ci_portafolio;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
